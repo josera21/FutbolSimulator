937e7b85532585f13fa5587eb56faa64b11a386b
Jose Camacaro <jochix21@gmail.com>
2017-12-03 13:48:00 -0400

    GUI added
    Added GUI with Pygamemenu library

--- equipo.py
+++ equipo.py
@@ -1,15 +1,20 @@
 import random
+import time
+import pygame
+from pygame.locals import *
 
 class Equipo(object):
 	"""docstring for Equipo"""
 
-	def __init__(self, nombre, ranking):
+	def __init__(self, nombre, ranking, formacion):
 		super(Equipo, self).__init__()
 		self.nombre = nombre
 		self.ranking = int(ranking)
+		self.formacion = formacion
+		self.pases_exitosos = 0 
 		self.goles = 0
 		self.fallidos = 0
-	
+
 	def cargar_probabilidades(self):
 		probabilidades = self.__porcentajes_ranking()
 
@@ -19,13 +24,13 @@ class Equipo(object):
 		self.prob_pase = probabilidades['pase']
 
 	def __porcentajes_ranking(self):
-		if 1 <= self.ranking <= 10:
+		if self.ranking == 1:
 			return {'ganar': 0.75, 'anotar': 0.60, 'encajar': 0.20, 'pase':0.60}
-		elif 11 <= self.ranking <= 20:
+		elif self.ranking == 2:
 			return {'ganar': 0.60, 'anotar': 0.50, 'encajar': 0.30, 'pase':0.50}
-		elif 21 <= self.ranking <= 30:
+		elif self.ranking == 3:
 			return {'ganar': 0.50, 'anotar': 0.40, 'encajar': 0.40, 'pase':0.40}
-		elif 31 <= self.ranking <= 40:
+		elif self.ranking == 4:
 			return {'ganar': 0.40, 'anotar': 0.30, 'encajar': 0.45, 'pase':0.35}
 		else:
 			return {'ganar': 0.30, 'anotar': 0.25, 'encajar': 0.50, 'pase':0.30}
@@ -36,9 +41,10 @@ class Equipo(object):
 
 		while exitoso and cont_pase < 4:
 			pase = random.randint(0,100) + (self.prob_pase * 100)
-
+			
 			if pase >= 100:
 				cont_pase = cont_pase + 1
+				self.__actualizar_pases()
 			else:
 				exitoso = False
 				return False
@@ -59,6 +65,9 @@ class Equipo(object):
 
 	def __actualizar_remates_fuera(self):
 		self.fallidos = self.fallidos + 1
+
+	def __actualizar_pases(self):
+		self.pases_exitosos = self.pases_exitosos + 1
 
 	def mostrar_estadisticas(self):
 		print("Goles: ", self.goles, "\n Fallidos: ", self.fallidos)
---
+++ pygameMenu/config_controls.py
@@ -0,0 +1,28 @@
+# coding=utf-8
+"""
+CONTROLS
+Default controls of Menu object.
+
+Copyright (C) 2017 Pablo Pizarro @ppizarror
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+"""
+
+# Import locals
+import pygame.locals as __locals
+
+# Controls
+MENU_CTRL_BACK = __locals.K_BACKSPACE
+MENU_CTRL_CLOSE_MENU = __locals.K_ESCAPE
+MENU_CTRL_DOWN = __locals.K_UP
+MENU_CTRL_ENTER = __locals.K_RETURN
+MENU_CTRL_LEFT = __locals.K_LEFT
+MENU_CTRL_RIGHT = __locals.K_RIGHT
+MENU_CTRL_UP = __locals.K_DOWN
